4c3f66fcb5a31816e38f0fb01d3eeb452c525c98
Joao-Pedro-Fiuza-Wilges <jpfwilges@gmail.com>
2025-09-01 21:54:36 -0300

    Add files via upload

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+// Fazendo café
+primeiroPasso = "1. Coloque água na chaleira \n"
+segundoPasso = "2. Aqueça até ferver \n"
+terceiroPasso = "3. Coloque café no filtro \n"
+quartoPasso = "4. Despeje água quente no café \n"
+quintoPasso = "5. Coloque açucar \n"
+sextoPasso = "6. Sirva-se"
+
+// utilizando o console.log para exibir as informações
+console.log(primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso,sextoPasso,)
---
+++ bolo.js
@@ -0,0 +1,18 @@
+// Fazendo bolo de 03 camadas
+primeiroPasso = "Ingredientes: \n \n"
+segundoPasso = "Masssa de bolo red velvet (3 camadas) \n"
+terceiroPasso = "300 g de chocolate meio amargo \n"
+quartoPasso = "200 g de creme de leite fresco \n"
+quintoPasso = "1 vidro de cerejas em calda \n"
+sextoPasso = "50 ml de licor de cereja \n"
+setimoPasso = "2 colheres de sopa de manteiga \n\n"
+oitavoPasso = "Preparo: \n\n"
+nonoPasso = "Massa: faça a massa do red velvet, dividida em 3 formas iguais a esse \n"
+decimoPasso = "Ganache: derreta o chocolate, misture com creme de leite de manteiga. Acrescente licor \n"
+decimoPrimeiroPasso = "Cerejas: pique algumas cerejas e guarde algumas inteiras para decorar \n\n"
+decimoSegundoPasso = "Montagem: \n\n"
+decimoTerceiroPasso = "Coloque a primeira camada, regue com calda das cerejas e licor, espalhe o ganache e cerejas picadas. Repita até a terceira camada \n"
+decimoQuartoPasso = "Cubra com o ganache restante e decore com as cerejas restantes \n\n"
+decimoQuintoPasso = "Sirva-se \n"
+// utilizando o console.log para exibir as informações
+console.log(primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso,sextoPasso,setimoPasso,oitavoPasso,nonoPasso,decimoPrimeiroPasso,decimoSegundoPasso,decimoTerceiroPasso,decimoQuartoPasso,decimoQuintoPasso)
